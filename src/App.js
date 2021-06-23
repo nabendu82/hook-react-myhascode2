58404b6bca68c49a4fdb3fce70ef90fbb78da28d
Nabendu <nabendu.biswas@gmail.com>
2021-06-23 20:48:24 +0530

    Day 11 done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import React from 'react';
 import './App.css';
-import MouseContainer from './components/MouseContainer'
+// import GrandParent from './components/GrandParent'
+import FocusInput from './components/FocusInput';
+// export const UserContext = React.createContext();
+// export const ChannelContext = React.createContext();
 
 function App() {
   return (
     <div className="App">
-      <MouseContainer />
+      {/* <UserContext.Provider value={'Nabendu'}>
+        <ChannelContext.Provider value={'TheWebDev'}>
+          <GrandParent />
+        </ChannelContext.Provider>
+      </UserContext.Provider> */}
+      <FocusInput />
     </div>
   );
 }
